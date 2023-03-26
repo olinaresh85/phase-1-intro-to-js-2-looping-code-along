8505ac3bdbf0c1c5cafb038bf22d28f72afb1dc6
Naresh Oli <olinaresh85@gmail.com>
2023-03-25 22:54:07 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+    let msgArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        let message = `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`;
+        msgArray.push(message);
+    }
+    return msgArray;
+}
+
+function countDown(){
+    for (let i=10;i>=0;i--){
+        console.log(i);
+    }
+}
+
